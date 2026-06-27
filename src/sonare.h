434fe0647b25d880d2c93e1ddf907ebb03dcdb42
libraz <libraz@libraz.net>
2026-06-28 07:28:11 +0900

    release: post-bump fixes for v1.4.1

--- src/sonare.h
+++ src/sonare.h
@@ -7,8 +7,8 @@
 // Version information
 #define SONARE_VERSION_MAJOR 1
 #define SONARE_VERSION_MINOR 4
-#define SONARE_VERSION_PATCH 0
-#define SONARE_VERSION_STRING "1.4.0"
+#define SONARE_VERSION_PATCH 1
+#define SONARE_VERSION_STRING "1.4.1"
 
 // Utility
 #include "util/exception.h"
